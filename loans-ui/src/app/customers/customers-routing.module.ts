7a6911c89832bc73e72a42ddb7f1836b85ebd0c8
Ishita <2018ceb1008@iitrpr.ac.in>
2023-09-05 03:47:10 +0530

    Ui static html pages for all the components

--- loans-ui/src/app/customers/customers-routing.module.ts
+++ loans-ui/src/app/customers/customers-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomersComponent } from './customers.component';
+import { ListCustomersComponent } from './list-customers/list-customers.component';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { DeleteCustomerComponent } from './delete-customer/delete-customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { SearchCustomerComponent } from './search-customer/search-customer.component';
 
-const routes: Routes = [{ path: '', component: CustomersComponent }];
+const routes: Routes = [
+  { path: '', component: ListCustomersComponent },
+  { path: 'add', component: AddCustomerComponent },
+  { path: 'delete/:id', component: DeleteCustomerComponent },
+  { path: 'edit/:id', component: EditCustomerComponent },
+  { path: 'search', component: SearchCustomerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- loans-ui/src/app/customers/customers.module.ts
+++ loans-ui/src/app/customers/customers.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent } from './customers.component';
+import { ListCustomersComponent } from './list-customers/list-customers.component';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { DeleteCustomerComponent } from './delete-customer/delete-customer.component';
+import { SearchCustomerComponent } from './search-customer/search-customer.component';
 
 
 @NgModule({
   declarations: [
-    CustomersComponent
+    CustomersComponent,
+    ListCustomersComponent,
+    AddCustomerComponent,
+    EditCustomerComponent,
+    DeleteCustomerComponent,
+    SearchCustomerComponent
   ],
   imports: [
     CommonModule,
